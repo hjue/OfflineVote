c3857de6dead33e914c2387c9618277144626aaf
hjue <haojue@gmail.com>
2016-02-19 17:01:36 +0800

    added icon

--- js/page/home.ios.js
+++ js/page/home.ios.js
@@ -2,12 +2,19 @@
 
 var React = require('react-native');
 
+var Icon = require('react-native-vector-icons/FontAwesome');
+
 var {
   StyleSheet,
   Text,
   View,
+  PanResponder,
+  Animated,
+  TouchableHighlight,
+  TouchableWithoutFeedback
 } = React;
 
+var TouchableHighlight = require('TouchableHighlight');
 var TouchableWithoutFeedback = require('TouchableWithoutFeedback');
 
 
@@ -24,13 +31,15 @@ var Button = React.createClass({
         count: 0,
     };
   },
+
   render: function() {
     return (
-      <TouchableWithoutFeedback onPress={this._handlePress}>
+      <TouchableWithoutFeedback onPress={this._handlePress} >
         <View style={this.style()}>
         <Text style={{color:'#fff',fontSize: 24,textAlign: 'center',marginBottom: 50,}}>
   {this.state.count}
         </Text>
+        <Icon name={this.props.icon} size={70} color="#fff" style={{textAlign: 'center',marginBottom: 50}} />
         <Text style={{color:'#fff',fontSize: 24,textAlign: 'center',}}>
   {this.props.title}
         </Text>
@@ -47,13 +56,24 @@ var Button = React.createClass({
 });
 
 var home = React.createClass({
-
+  // componentWillMount: function() {
+  //  this._panResponder = PanResponder.create({
+  //       onStartShouldSetPanResponder: (evt, gestureState) => true,
+  //       onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+  //       onMoveShouldSetPanResponder: (evt, gestureState) => true,
+  //       onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+  //       onPanResponderGrant: () => this.setState({scroll: false}),
+  //       onPanResponderMove: console.log("move"),
+  //         onPanResponderRelease: () => this.setState({scroll: true})
+  //     })
+  // },
+  //{...this._panResponder.panHandlers}
   render: function() {
     return (
-        <View style={{flexDirection:'row',marginTop:20,flex: 1,}}>
-          <Button backgroundColor="#27AE60" title="满意" ></Button>
-          <Button backgroundColor="#C0392C" title="一般"></Button>
-          <Button backgroundColor="#F1C40E" title="不满意"></Button>
+        <View style={{flexDirection:'row',marginTop:0,flex: 1,}} >
+          <Button backgroundColor="#27AE60" title="满意" icon="smile-o" ></Button>
+          <Button backgroundColor="#C0392C" title="一般" icon="meh-o" ></Button>
+          <Button backgroundColor="#F1C40E" title="不满意" icon="frown-o" ></Button>
         </View>
     );
   },
